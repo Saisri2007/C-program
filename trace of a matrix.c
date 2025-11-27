306670615f04fbe02ad79f46436d04f4239b6983
Saisri2007 <saisrilaxmi7735@gmail.com>
2025-11-27 22:20:10 +0530

    Add files via upload

---
+++ trace of a matrix.c
@@ -0,0 +1,20 @@
+/*write a c program to find the trace of a matrix*/
+#include<stdio.h>
+int main()
+{
+	int mat[3][3],sum=0,i,j;
+	printf("\nEnter the elements of Matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&mat[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		sum=sum+mat[i][i];	
+	}
+	printf("\nThe trace of a given matrix is %d",sum);
+
+}
